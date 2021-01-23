e52b2387647b56a87e158926ac152204dee2e58a
Aysun CAG YILMAZKULAS <63105388+AysunCagYilmazkulas@users.noreply.github.com>
2021-01-23 18:34:00 +0300

    Add files via upload

--- RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/Form1.cs
+++ RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RSA_Algoritması_Uygulaması
@@ -15,54 +9,67 @@ namespace RSA_Algoritması_Uygulaması
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); //Constructor
         }
 
-        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        public static byte[] Encryption(byte[] girilenVeri, RSAParameters RSAKey, bool EnIyiAsimetrikSifrelemePadding)
         {
+            // Encryption adli byte arrayi dondurecek olan statik fonksiyon acilmistir.
+            // Bu fonksiyon sifrelenecek veri, System.Security.Cryptography kutuphanesine bagli anahtar ve OAEP bool degerlerini alir.
+
             try
             {
-                byte[] encryptedData;
+                // Sifrelenecek veri alinir. System.Security.Cryptography kutuphanesi kullanilarak sifreleme yapilir. Sifrelenmis veri dondurulur.
+
+                byte[] verininSifrelenmisHali; 
                 using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
                 {
-                    RSA.ImportParameters(RSAKey);
-                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                    RSA.ImportParameters(RSAKey); 
+                    verininSifrelenmisHali = RSA.Encrypt(girilenVeri, EnIyiAsimetrikSifrelemePadding);
                 }
-                return encryptedData;
+                return verininSifrelenmisHali;
             }
-            catch (CryptographicException e)
+            catch (CryptographicException e) // Try kisim dogru calismadigi takdirde CryptographicException classindaki e mesajini gosterir.
             {
                 Console.WriteLine(e.Message);
                 return null;
             }
         }
 
-        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        public static byte[] Decryption(byte[] girilenVeri, RSAParameters RSAKey, bool EnIyiAsimetrikSifrelemePadding)
         {
+            // Decryption adli byte arrayi dondurecek olan statik fonksiyon acilmistir.
+            // Bu fonksiyon sifrelenecek veri, System.Security.Cryptography kutuphanesine bagli anahtar ve OAEP bool degerlerini alir.
+
             try
             {
-                byte[] decryptedData;
+                // Desifrelenecek veri alinir. System.Security.Cryptography kutuphanesi kullanilarak desifreleme yapilir. Desifrelenmis veri dondurulur.
+
+                byte[] verininDesifrelenmisHali;
                 using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
                 {
                     RSA.ImportParameters(RSAKey);
-                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                    verininDesifrelenmisHali = RSA.Decrypt(girilenVeri, EnIyiAsimetrikSifrelemePadding);
                 }
-                return decryptedData;
+                return verininDesifrelenmisHali;
             }
-            catch (CryptographicException e)
+            catch (CryptographicException e) // Try kisim dogru calismadigi takdirde CryptographicException classindaki e mesajini gosterir.
             {
                 Console.WriteLine(e.ToString());
                 return null;
             }
         }
 
-        UnicodeEncoding ByteConverter = new UnicodeEncoding();
-        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        UnicodeEncoding ByteConverter = new UnicodeEncoding(); // Bayt dizisi ve string arasında dönüştürmek için bir Unicode Kodlayıcı oluşturulur.
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(); // Yeni bir RSACryptoServiceProvider örneği oluşturulur.
         byte[] plaintext;
         byte[] encryptedtext;
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Orijinal, şifrelenmiş ve şifresi çözülmüş verileri tutmak için plaintext adli bayt dizisi oluşturulur.
+            // Yukaridaki Encryption fonksiyonu kullanilarak encryptedtext txtencrypt'e aktarilir.
+
             plaintext = ByteConverter.GetBytes(txtplain.Text);
             encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
             txtencrypt.Text = ByteConverter.GetString(encryptedtext);
@@ -70,9 +77,12 @@ namespace RSA_Algoritması_Uygulaması
 
         private void button2_Click_1(object sender, EventArgs e)
         {
-            byte[] decryptedtex = Decryption(encryptedtext,
+            // Aktarılan bayt dizisinin şifresini çözen decryptedtex byte dizisi tanimlanir.
+            // Yukaridaki Decryption fonksiyonu kullanilarak decryptedtext txtdecrypt'e aktarilir.
+
+            byte[] decryptedtext = Decryption(encryptedtext,
             RSA.ExportParameters(true), false);
-            txtdecrypt.Text = ByteConverter.GetString(decryptedtex);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtext);
         }
     }
 }
