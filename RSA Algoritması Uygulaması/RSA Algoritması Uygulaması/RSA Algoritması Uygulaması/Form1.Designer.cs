313d20f491469efbb8eafc66e33c4134f5ef7b83
Aysun CAG YILMAZKULAS <63105388+AysunCagYilmazkulas@users.noreply.github.com>
2021-01-23 18:23:54 +0300

    Add files via upload
    Kod üzerinde güncelleme yapılmıştır.

---
+++ RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace RSA_Algoritması_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtplain = new System.Windows.Forms.TextBox();
+            this.txtencrypt = new System.Windows.Forms.TextBox();
+            this.txtdecrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(314, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(618, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RSA Asimetrik Şifreleme Algoritması";
+            // 
+            // txtplain
+            // 
+            this.txtplain.Location = new System.Drawing.Point(141, 188);
+            this.txtplain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtplain.Multiline = true;
+            this.txtplain.Name = "txtplain";
+            this.txtplain.Size = new System.Drawing.Size(584, 135);
+            this.txtplain.TabIndex = 1;
+            // 
+            // txtencrypt
+            // 
+            this.txtencrypt.Location = new System.Drawing.Point(141, 422);
+            this.txtencrypt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtencrypt.Multiline = true;
+            this.txtencrypt.Name = "txtencrypt";
+            this.txtencrypt.Size = new System.Drawing.Size(584, 135);
+            this.txtencrypt.TabIndex = 2;
+            // 
+            // txtdecrypt
+            // 
+            this.txtdecrypt.Location = new System.Drawing.Point(141, 648);
+            this.txtdecrypt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtdecrypt.Multiline = true;
+            this.txtdecrypt.Name = "txtdecrypt";
+            this.txtdecrypt.Size = new System.Drawing.Size(1119, 135);
+            this.txtdecrypt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(989, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(272, 135);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Şifrele";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(989, 422);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(272, 135);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Şifre Çöz";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(135, 151);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Şifrelenecek Düz Metin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(135, 385);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Şifrelenmiş Metin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(135, 610);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(420, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Şifre Çözme Sonrası Düz Metin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1397, 817);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtdecrypt);
+            this.Controls.Add(this.txtencrypt);
+            this.Controls.Add(this.txtplain);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Asimetrik Şifreleme Örneği";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtplain;
+        private System.Windows.Forms.TextBox txtencrypt;
+        private System.Windows.Forms.TextBox txtdecrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/RSA Algoritması Uygulaması/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSA_Algoritması_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); //Constructor
+        }
+
+        public static byte[] Encryption(byte[] girilenVeri, RSAParameters RSAKey, bool EnIyiAsimetrikSifrelemePadding)
+        {
+            // Encryption adli byte arrayi dondurecek olan statik fonksiyon acilmistir.
+            // Bu fonksiyon sifrelenecek veri, System.Security.Cryptography kutuphanesine bagli anahtar ve OAEP bool degerlerini alir.
+
+            try
+            {
+                // Sifrelenecek veri alinir. System.Security.Cryptography kutuphanesi kullanilarak sifreleme yapilir. Sifrelenmis veri dondurulur.
+
+                byte[] verininSifrelenmisHali; 
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey); 
+                    verininSifrelenmisHali = RSA.Encrypt(girilenVeri, EnIyiAsimetrikSifrelemePadding);
+                }
+                return verininSifrelenmisHali;
+            }
+            catch (CryptographicException e) // Try kisim dogru calismadigi takdirde CryptographicException classindaki e mesajini gosterir.
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static byte[] Decryption(byte[] girilenVeri, RSAParameters RSAKey, bool EnIyiAsimetrikSifrelemePadding)
+        {
+            // Decryption adli byte arrayi dondurecek olan statik fonksiyon acilmistir.
+            // Bu fonksiyon sifrelenecek veri, System.Security.Cryptography kutuphanesine bagli anahtar ve OAEP bool degerlerini alir.
+
+            try
+            {
+                // Desifrelenecek veri alinir. System.Security.Cryptography kutuphanesi kullanilarak desifreleme yapilir. Desifrelenmis veri dondurulur.
+
+                byte[] verininDesifrelenmisHali;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    verininDesifrelenmisHali = RSA.Decrypt(girilenVeri, EnIyiAsimetrikSifrelemePadding);
+                }
+                return verininDesifrelenmisHali;
+            }
+            catch (CryptographicException e) // Try kisim dogru calismadigi takdirde CryptographicException classindaki e mesajini gosterir.
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        UnicodeEncoding ByteConverter = new UnicodeEncoding(); // Bayt dizisi ve string arasında dönüştürmek için bir Unicode Kodlayıcı oluşturulur.
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(); // Yeni bir RSACryptoServiceProvider örneği oluşturulur.
+        byte[] plaintext;
+        byte[] encryptedtext;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Orijinal, şifrelenmiş ve şifresi çözülmüş verileri tutmak için plaintext adli bayt dizisi oluşturulur.
+            // Yukaridaki Encryption fonksiyonu kullanilarak encryptedtext txtencrypt'e aktarilir.
+
+            plaintext = ByteConverter.GetBytes(txtplain.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            txtencrypt.Text = ByteConverter.GetString(encryptedtext);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            // Aktarılan bayt dizisinin şifresini çözen decryptedtex byte dizisi tanimlanir.
+            // Yukaridaki Decryption fonksiyonu kullanilarak decryptedtext txtdecrypt'e aktarilir.
+
+            byte[] decryptedtext = Decryption(encryptedtext,
+            RSA.ExportParameters(true), false);
+            txtdecrypt.Text = ByteConverter.GetString(decryptedtext);
+        }
+    }
+}
